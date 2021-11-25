d070c594eebca78ba86bc36f72f0d9794ca91635
linyuhc001 <linyuhc001@gmail.com>
2021-11-25 22:59:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -255,6 +255,7 @@ CONFIG_PACKAGE_luci-app-unblockneteasemusic=y #解锁网易云灰色歌曲
 #CONFIG_UnblockNeteaseMusic_Go=y
 #CONFIG_UnblockNeteaseMusic_NodeJS=y
 #CONFIG_PACKAGE_luci-i18n-unblockmusic-zh-cn=y
+# CONFIG_PACKAGE_luci-app-turboacc is not set
 # CONFIG_PACKAGE_luci-app-airplay2 is not set #Apple AirPlay2音频接收服务器
 # CONFIG_PACKAGE_luci-app-music-remote-center is not set #PCHiFi数字转盘遥控
 # CONFIG_PACKAGE_luci-app-usb-printer is not set #USB打印机
